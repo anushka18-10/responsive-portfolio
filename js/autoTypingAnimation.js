b448bf587c5845209b3d7c9e998f09ccf0be823b
anushka18-10a <work.anushkamandwade@gmail.com>
2026-02-05 21:24:07 +0530

    Initial portfolio project

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Anushka Mandwade</span> a passionate Frontend Developer and UI/UX Designer who enjoys creating clean, responsive, and user-friendly web experiences. I focus on building visually appealing interfaces that combine modern design with smooth functionality, ensuring every project delivers a great user experience. <br><br> With a strong interest in both design and development, I love turning ideas into interactive digital products. I pay close attention to usability, accessibility, and detail while working with modern web technologies and design tools. My goal is to create websites that are not only attractive but also intuitive, efficient, and impactful."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
